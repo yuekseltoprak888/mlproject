781a0417fa3a2f65b0493cec485f0e49600a48bd
iOSEngineer2023 <yueksel.toprak@galexis.com>
2024-07-08 12:43:26 +0200

    third commit

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+import traceback
+
+def error_message_detail(error, error_detail: sys):
+    # Get the exception info
+    _, _, exc_tb = sys.exc_info()
+    
+    # Extract the file name and line number from the traceback
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    
+    # Create the error message
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(file_name, line_number, str(error))
+    
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
