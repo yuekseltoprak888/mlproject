ec2fc92dc6a3c355ae46c0b89b7d9cd50acf211f
iOSEngineer2023 <yueksel.toprak@galexis.com>
2024-07-09 15:01:08 +0200

    fourth commit ad exception and logger

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,21 @@
 import sys
 import traceback
 import logging
+from datetime import datetime
+import os
+
+
+# Configure logging
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s - %(levelname)s - %(message)s - %(filename)s:%(lineno)d',
+    level=logging.INFO
+)
 
 def error_message_detail(error, error_detail: sys):
     # Get the exception info
@@ -22,11 +37,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-if __name__== "__main__":
-
-    try:
-        a =  1 / 0
-    except Exception as e:
-        logging.info("Loggin has started")
-        raise CustomException(e, sys)
